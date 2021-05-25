e4755c50145c9cfa087050d1890077e45f53d683
Raphaël Mouneyres <rmouneyres@gmail.com>
2021-05-25 20:07:24 +0200

    #40 mididing output filter

--- configs/passthru.py
+++ configs/passthru.py
@@ -1,4 +1,12 @@
 from mididings import *
 
+config(
+	in_ports = [
+		'from_hw',
+		'from_aeolus',],
+	out_ports = [
+		'to_aeolus',
+		'to_hw',],
+)
 # simple script to passthru midi from input to output
 run(Pass())
