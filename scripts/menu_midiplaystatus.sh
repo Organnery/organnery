83dd988e0fbe3e2822b3e0f6b52f2a7ac4cb33f2
Raphaël Mouneyres <rmouneyres@gmail.com>
2021-05-25 21:12:04 +0200

    #46 add rec/play status in menu

--- scripts/menu_midiplaystatus.sh
+++ scripts/menu_midiplaystatus.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 if [[ `pgrep -f aplaymidi` ]]; then
-    echo "Currently Playing"
+	echo "<openbox_pipe_menu>";
+	echo "  <item label=\"Currently playing\">";
+	echo "    <action name=\"Execute\">";
+	echo "      <execute>";
+	echo "      </execute>";
+	echo "    </action>";
+	echo "  </item>";
+	echo "</openbox_pipe_menu>";
     exit 1
 else
-    echo "NOT Playing"
+	echo "<openbox_pipe_menu>";
+	echo "  <item label=\"NOT playing\">";
+	echo "    <action name=\"Execute\">";
+	echo "      <execute>";
+	echo "      </execute>";
+	echo "    </action>";
+	echo "  </item>";
+	echo "</openbox_pipe_menu>";
     exit 0
 fi
--- scripts/menu_midirecstatus.sh
+++ scripts/menu_midirecstatus.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 if [[ `pgrep -f arecordmidi` ]]; then
-    echo "Currently Recording"
+	echo "<openbox_pipe_menu>";
+	echo "  <item label=\"Currently Recording\">";
+	echo "    <action name=\"Execute\">";
+	echo "      <execute>";
+	echo "      </execute>";
+	echo "    </action>";
+	echo "  </item>";
+	echo "</openbox_pipe_menu>";
     exit 1
 else
-    echo "NOT Recording"
+	echo "<openbox_pipe_menu>";
+	echo "  <item label=\"NOT Recording\">";
+	echo "    <action name=\"Execute\">";
+	echo "      <execute>";
+	echo "      </execute>";
+	echo "    </action>";
+	echo "  </item>";
+	echo "</openbox_pipe_menu>";
     exit 0
 fi
